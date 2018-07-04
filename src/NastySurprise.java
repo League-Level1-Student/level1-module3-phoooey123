03da99462ee772934a6fd538f46f881e8a7f8335
league <league@wts14-4.attlocal.net>
2018-07-03 17:02:00 -0700

    Finished NastySurprise and SimonSays.

--- src/NastySurprise.java
+++ src/NastySurprise.java
@@ -8,11 +8,13 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class NastySurprise implements ActionListener {
 	JFrame f = new JFrame();
 	JButton b1 = new JButton();
 	JButton b2 = new JButton();
+	JPanel p = new JPanel();
 
 	// Create Panel for buttons.
 	public static void main(String[] args) {
@@ -22,13 +24,15 @@ public class NastySurprise implements ActionListener {
 
 	void frame() {
 		f.setVisible(true);
-		f.add(b1);
-		f.add(b2);
+		f.add(p);
+		p.add(b1);
+		p.add(b2);
 		b1.setText("Trick");
 		b2.setText("Treat");
 		b1.addActionListener(this);
 		b2.addActionListener(this);
 		f.pack();
+		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
 	}
 
 	private void showPictureFromTheInternet(String imageUrl) {
@@ -48,6 +52,13 @@ public class NastySurprise implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-
+		if (e.getSource() == b2) {
+			showPictureFromTheInternet(
+					"https://media.istockphoto.com/photos/sunny-cat-picture-id508030340?k=6&m=508030340&s=612x612&w=0&h=ri9QYzvZkiuniJPaInHQfW6z1JNNzsNOAWEiLLSTX5o=");
+		}
+		if (e.getSource() == b1) {
+			showPictureFromTheInternet(
+					"https://previews.123rf.com/images/lightwise/lightwise1608/lightwise160800108/63825857-halloween-pumpkin-and-scary-jack-o-lantern-character-in-a-side-view-with-an-open-mouth-on-a-white-ba.jpg");
+		}
 	}
 }
--- src/SimonSays.java
+++ src/SimonSays.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2018
  *    Level 1
@@ -10,86 +11,109 @@ import java.net.URL;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Random;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import java.io.IOException;
 
 public class SimonSays extends KeyAdapter {
 
 	// Complete steps 1 - 7 before you test
 	// 1. Declare a JFrame variable
-	
+	JFrame f;
+
 	HashMap<Integer, String> images = new HashMap<Integer, String>();
 	private int imageIndex;
 	private int tries = 0;
 	private boolean simonSays = false;
-	Date timeAtStart;
+	static Date timeAtStart = new Date();
+	Date timeAtEnd = new Date();
 
 	private void makeAlbum() {
-		// 2. Add the four images that match keyboard keys like this: 
-		//images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
+		// 2. Add the four images that match keyboard keys like this:
+		images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
+		images.put(new Integer(KeyEvent.VK_RIGHT), "right.jpg");
+		images.put(new Integer(KeyEvent.VK_DOWN), "down.jpg");
+		images.put(new Integer(KeyEvent.VK_LEFT), "left.jpg");
 
 		// 3. Use a JOptionPane to tell the user the rules: "Press the matching key when
 		// 'Simon says' otherwise press a different key"
-		
+		JOptionPane.showMessageDialog(null, "Press the matching key when 'Simon says' otherwise press a different key");
 		// 4. Call the showImage method to show an image
+		showImage();
 	}
 
 	public void keyPressed(KeyEvent e) {
 		// 15. Make a points variable to track the score.
-
+		int points = 0;
 		// 16. If the keyCode matches the imageIndex and "Simon says"
-		
+		if (e.getKeyCode() == imageIndex && simonSays == true) {
+
 			// 17. Increase the value of score
-		
+			points++;
 			// 18. Use the speak method to tell the user they were correct
-		
+			speak("Correct");
+		} else if (e.getKeyCode() != imageIndex && !simonSays) {
+			points++;
+			speak("Correct");
+		} else {
+			speak("Incorrect");
+		}
 		// 19. If the keyCode doesn't match the imageIndex and "Simon didn't say..."
-		
-			// 20.  Increase the value of score
-		
-			// 21. Use the speak method to tell the user they were correct
-		
+
+		// 20. Increase the value of score
+
+		// 21. Use the speak method to tell the user they were correct
+
 		// 22. Increment tries by 1
-		
+		tries++;
 		// 25. If tries is greater than 9 (or however many you want)...
-		
+		if (tries > 9) {
 			// 26. Tell the user their score
-		
+			JOptionPane.showMessageDialog(null, "Your score is " + points);
 			// 27. Exit the program
 
+			System.out.println((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000);
+			System.exit(0);
+		}
 		// 23. Dispose of the frame
-
+		f.dispose();
 		// 24. Call the showImage method to show a new image
+		showImage();
 	}
 
 	private void showImage() {
 		// 5. Initialize your frame to a new JFrame()
-		
+		f = new JFrame();
 		// 6. Set the frame to visible
-
+		f.setVisible(true);
 		// 7. Uncomment the following line to add a random image to your frame
-		//frame.add(getNextRandomImage());
+		f.add(getNextRandomImage());
 
 		// 8. Set the name of your frame
-
+		f.setName("Simon Says");
 		// 9. Pack the frame
-		
-		// 10.  Set the defaultCloseOperation of your from to JFRAME.EXIT_ON_CLOSE
-		
+		f.pack();
+		// 10. Set the defaultCloseOperation of your from to JFRAME.EXIT_ON_CLOSE
+		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
 		// 11. Add a key listener to the frame
-
+		f.addKeyListener(this);
 		// 12. Create a new instance of Random
-
-		// 13. Use the Random and the speak method to either say 
+		Random r = new Random();
+		// 13. Use the Random and the speak method to either say
 		// "Simon says press this key" or "Press this key"
-
+		int simon = r.nextInt(2);
+		if (simon == 1) {
+			simonSays = true;
+			speak("Simon says press this key");
+		} else {
+			simonSays = false;
+			speak("Press this key");
+		}
 		// 14. Set the value of simonSays to true/false appropriately
 
-		
 	}
 
 	private Component getNextRandomImage() {
@@ -113,17 +137,15 @@ public class SimonSays extends KeyAdapter {
 
 	public static void main(String[] args) throws Exception {
 		new SimonSays().makeAlbum();
+
 	}
 }
 
 /*
- * BONUS!
- * Add a timer :
- * ~~~ where the code starts running ~~~ 
- * timeAtStart = newDate();
+ * BONUS! Add a timer : ~~~ where the code starts running ~~~ timeAtStart =
+ * newDate();
  *
- * ~~~ where the code ends ~~~ 
- * Date timeAtEnd = new Date();
+ * ~~~ where the code ends ~~~ Date timeAtEnd = new Date();
  * System.out.println((timeAtEnd.getTime()-timeAtStart.getTime())/1000);
  * System.exit(0);
  */
